e883c0450480b84d272ce27f503ae0bea146c1cd
Christopher Morris <christopher.morris@udo.edu>
2024-01-01 16:30:52 +0100

    Implemented WLOA variants.

--- kernel/tud.py
+++ kernel/tud.py
@@ -15,7 +15,8 @@ for i in range(3, 7):
     cliques.append(create_clique(i))
 
 datasets = ["ENZYMES", "MUTAG", "PROTEINS", "PTC_FM", "PTC_MR", "NCI1"]  # , "Mutagenicity",  "MCF-7",]
-# datasets = ["MUTAG", "PROTEINS", "PTC_FM", "PTC_MR", "NCI1"] #, "Mutagenicity",  "MCF-7",]
+#datasets = ["ENZYMES"] #, "Mutagenicity",  "MCF-7",]
+datasets = ["MUTAG"]  # , "Mutagenicity",  "MCF-7",]
 
 # 1-WL.
 for ds in datasets:
@@ -52,6 +53,25 @@ for ds in datasets:
         print("#")
 print("###")
 
+for ds in datasets:
+    print(ds)
+
+    for s in range(1, (len(cycles) + 1)):
+        gram_matrices = []
+        for i in range(1, 6):
+            graph_db, classes = read_txt(ds)
+            gram_matrix = compute_wl_f(graph_db, cliques[0:s], i, induced=True, compute_gram=False)
+            gram_matrix = normalize_feature_vector_dense(gram_matrix)
+            gram_matrices.append(gram_matrix)
+
+        acc, std, mrg, mrg_std = linear_svm_evaluation(gram_matrices, classes, num_iter=1000,
+                                                       num_repetitions=10)  # , C=[10 ** 10])
+        print(acc, std, mrg, mrg_std)
+        print("#")
+print("###")
+
+exit()
+
 # 1-WLOA.
 for ds in datasets:
     print(ds)
@@ -78,6 +98,22 @@ for ds in datasets:
         for i in range(1, 6):
             graph_db, classes = read_txt(ds)
             gram_matrix = compute_wloa_f(graph_db, cycles[0:s], i, induced=True)
+            gram_matrix = normalize_gram_matrix(gram_matrix)
+            gram_matrices.append(gram_matrix)
+
+        acc, std = kernel_svm_evaluation(gram_matrices, classes, num_repetitions=10)  # , C=[10 ** 10])
+        print(acc, std)
+        print("#")
+print("###")
+
+for ds in datasets:
+    print(ds)
+
+    for s in range(1, (len(cycles) + 1)):
+        gram_matrices = []
+        for i in range(1, 6):
+            graph_db, classes = read_txt(ds)
+            gram_matrix = compute_wloa_f(graph_db, cliques[0:s], i, induced=True)
             gram_matrix = normalize_gram_matrix(gram_matrix)
             gram_matrices.append(gram_matrix)
 
