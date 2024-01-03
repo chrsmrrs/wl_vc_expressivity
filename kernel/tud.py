abbbff89be7ec5195fb30af42afc8d22aafaa10f
Christopher Morris <christopher.morris@udo.edu>
2024-01-03 11:44:02 +0100

    Implemented WLOA variants.

--- kernel/tud.py
+++ kernel/tud.py
@@ -17,61 +17,61 @@ for i in range(3, 7):
 datasets = ["ENZYMES", "MUTAG", "PROTEINS", "PTC_FM", "PTC_MR", "NCI1"]  # , "Mutagenicity",  "MCF-7",]
 #datasets = ["ENZYMES"] #, "Mutagenicity",  "MCF-7",]
 #datasets = ["PTC_MR"]  # , "Mutagenicity",  "MCF-7",]
-datasets = ["NCI1"]  # , "Mutagenicity",  "MCF-7",]
+#datasets = ["NCI1"]  # , "Mutagenicity",  "MCF-7",]
 
-# 1-WL.
+# # 1-WL.
+# for ds in datasets:
+#     print(ds)
+#
+#     gram_matrices = []
+#     for i in range(1, 6):
+#         graph_db, classes = read_txt(ds)
+#         gram_matrix = compute_wl(graph_db, i, compute_gram=False)
+#         gram_matrix = normalize_feature_vector_dense(gram_matrix)
+#         gram_matrices.append(gram_matrix)
+#
+#     acc, std, mrg, mrg_std = linear_svm_evaluation(gram_matrices, classes, num_iter=1000,
+#                                                    num_repetitions=10)  # , C=[10 ** 10])
+#     print(acc, std, mrg, mrg_std)
+#     print("#")
+# print("###")
+
+# 1-WL_F.
 for ds in datasets:
     print(ds)
 
-    gram_matrices = []
-    for i in range(1, 6):
-        graph_db, classes = read_txt(ds)
-        gram_matrix = compute_wl(graph_db, i, compute_gram=False)
-        gram_matrix = normalize_feature_vector_dense(gram_matrix)
-        gram_matrices.append(gram_matrix)
+    for s in range(1, (len(cycles) + 1)):
+        gram_matrices = []
+        for i in range(1, 6):
+            graph_db, classes = read_txt(ds)
+            gram_matrix = compute_wl_f(graph_db, cycles[0:s], i, induced=True, compute_gram=False)
+            gram_matrix = normalize_feature_vector_dense(gram_matrix)
+            gram_matrices.append(gram_matrix)
 
-    acc, std, mrg, mrg_std = linear_svm_evaluation(gram_matrices, classes, num_iter=1000,
-                                                   num_repetitions=10)  # , C=[10 ** 10])
-    print(acc, std, mrg, mrg_std)
-    print("#")
+        acc, std, mrg, mrg_std = linear_svm_evaluation(gram_matrices, classes, num_iter=1000,
+                                                       num_repetitions=10)  # , C=[10 ** 10])
+        print(acc, std, mrg, mrg_std)
+        print("#")
 print("###")
 
-# # 1-WL_F.
-# for ds in datasets:
-#     print(ds)
-#
-#     for s in range(1, (len(cycles) + 1)):
-#         gram_matrices = []
-#         for i in range(1, 6):
-#             graph_db, classes = read_txt(ds)
-#             gram_matrix = compute_wl_f(graph_db, cycles[0:s], i, induced=True, compute_gram=False)
-#             gram_matrix = normalize_feature_vector_dense(gram_matrix)
-#             gram_matrices.append(gram_matrix)
-#
-#         acc, std, mrg, mrg_std = linear_svm_evaluation(gram_matrices, classes, num_iter=1000,
-#                                                        num_repetitions=10)  # , C=[10 ** 10])
-#         print(acc, std, mrg, mrg_std)
-#         print("#")
-# print("###")
-#
-# for ds in datasets:
-#     print(ds)
-#
-#     for s in range(1, (len(cliques) + 1)):
-#         gram_matrices = []
-#         for i in range(1, 6):
-#             graph_db, classes = read_txt(ds)
-#             gram_matrix = compute_wl_f(graph_db, cliques[0:s], i, induced=True, compute_gram=False)
-#             gram_matrix = normalize_feature_vector_dense(gram_matrix)
-#             gram_matrices.append(gram_matrix)
-#
-#         acc, std, mrg, mrg_std = linear_svm_evaluation(gram_matrices, classes, num_iter=1000,
-#                                                        num_repetitions=10)  # , C=[10 ** 10])
-#         print(acc, std, mrg, mrg_std)
-#         print("#")
-# print("###")
-#
-# exit()
+for ds in datasets:
+    print(ds)
+
+    for s in range(1, (len(cliques) + 1)):
+        gram_matrices = []
+        for i in range(1, 6):
+            graph_db, classes = read_txt(ds)
+            gram_matrix = compute_wl_f(graph_db, cliques[0:s], i, induced=True, compute_gram=False)
+            gram_matrix = normalize_feature_vector_dense(gram_matrix)
+            gram_matrices.append(gram_matrix)
+
+        acc, std, mrg, mrg_std = linear_svm_evaluation(gram_matrices, classes, num_iter=1000,
+                                                       num_repetitions=10)  # , C=[10 ** 10])
+        print(acc, std, mrg, mrg_std)
+        print("#")
+print("###")
+
+exit()
 
 # # 1-WLOA.
 # for ds in datasets:
