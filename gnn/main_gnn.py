6f00b0693c4b8191d0ab8dd6f6663165a87548e1
Christopher Morris <christopher.morris@udo.edu>
2024-01-14 13:36:41 +0100

    Implemented WLOA variants.

--- gnn/main_gnn.py
+++ gnn/main_gnn.py
@@ -12,7 +12,7 @@ def main():
         acc_train, s_train, acc_test, s_test = gnn_evaluation(GIN, d, [1,2,3,4,5], [64], max_num_epochs=200, batch_size=128,
                                        start_lr=0.01, num_repetitions=num_reps, all_std=False)
 
-        print(acc_train, s_train, acc_test, s_test.acc_train - acc_test)
+        print(acc_train, s_train, acc_test, s_test, acc_train - acc_test)
 
 if __name__ == "__main__":
     main()
--- gnn/main_gnn_f.py
+++ gnn/main_gnn_f.py
@@ -21,14 +21,14 @@ def main():
         for s in range(1, (len(cycles) + 1)):
             acc_train, s_train, acc_test, s_test = gnn_evaluation(GIN, cycles[0:s], d, [1,2,3,4,5], [64], max_num_epochs=200, batch_size=128,
                                            start_lr=0.01, num_repetitions=num_reps, all_std=False)
-            print(acc_train, s_train, acc_test, s_test. acc_train-acc_test)
+            print(acc_train, s_train, acc_test, s_test, acc_train-acc_test)
 
     for d in datasets:
         print(d)
         for s in range(1, (len(cliques) + 1)):
             acc_train, s_train, acc_test, s_test = gnn_evaluation(GIN, cliques[0:s], d, [1,2,3,4,5], [64], max_num_epochs=200, batch_size=128,
                                            start_lr=0.01, num_repetitions=num_reps, all_std=False)
-            print(acc_train, s_train, acc_test, s_test. acc_train-acc_test)
+            print(acc_train, s_train, acc_test, s_test, acc_train-acc_test)
 
 if __name__ == "__main__":
     main()
