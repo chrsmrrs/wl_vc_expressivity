cbcbee441bddad3f2012411b15a158caef7eb03c
Christopher Morris <christopher.morris@udo.edu>
2024-01-20 22:52:34 +0100

    Plot stuff.

--- kernel/synthetic.py
+++ kernel/synthetic.py
@@ -1,34 +1,24 @@
-import matplotlib
-#matplotlib.use("TKAgg")
-#matplotlib.use("macOSX")
-from matplotlib import pyplot as plt
+# matplotlib.use("TKAgg")
+# matplotlib.use("macOSX")
 
 import math as m
 
 import graph_tool as gt
 import numpy as np
+import seaborn as sns
 from graph_tool.all import *
 
 from auxiliarymethods.graphs import create_cycle
 from auxiliarymethods.svm import linear_svm_evaluation
 from auxiliarymethods.svm import normalize_feature_vector_dense
-
 from wl import compute_wl, compute_wl_f
-
-
-
-import seaborn as sns
-import pandas as pd
-
-import numpy as np
 
 # for using latex in plt it requires one installation:
 # $ sudo apt install dvipng
-#rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
-#rc('text', usetex=True)
+# rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
+# rc('text', usetex=True)
 
 sns.set_theme(style="white")
-
 
 
 # Create dataset not linear separabel by 1-WL.
@@ -160,7 +150,6 @@ g_4.add_edge(a, c)
 g_4.add_edge(b, d)
 subgraphs.append(g_4)
 
-
 #
 # # First synthetic dataset, linear separability.
 # num_it = 6
@@ -196,8 +185,8 @@ subgraphs.append(g_4)
 # Some hyperparameters.
 num_it = 6
 induced = True
-ps = [0.050, 0.075, 0.100, 0.125, 0.150, 0.175, 0.200, 0.225, 0.250, 0.275, 0.30, 0.325, 0.350, 0.375, 0.400]
-#ps = [0.050, 0.075]
+ps = [0.050, 0.075, 0.100, 0.125, 0.150, 0.175, 0.200, 0.225, 0.250, 0.275, 0.30]
+# ps = [0.050, 0.075]
 # ts = [8, 16, 32]
 num_graphs = 1000
 num_vertices = 20
@@ -210,8 +199,7 @@ for p in ps:
         graph_db, classes = create_random_graphs(num_graphs, num_vertices, p, -1, f)
         datasets.append((graph_db, classes, p, f))
 
-
-data = np.zeros([0,3])
+data = np.zeros([0, 3])
 
 results = []
 for (graph_db, classes, p, f) in datasets:
@@ -228,43 +216,29 @@ for (graph_db, classes, p, f) in datasets:
         #                                                C=[10 ** 10])
 
         acc_train, acc_test, mrg = linear_svm_evaluation(gram_matrices, classes, num_iter=1000, num_repetitions=10,
-                                                       C=[10 ** 10])
+                                                         C=[10 ** 10], all=False)
 
-        mrg = mrg.mean()
+        # mrg = mrg.mean()
 
         acc_train = np.reshape(acc_train, [10, 1])
         acc_test = np.reshape(acc_test, [10, 1])
-        mrg = np.reshape(np.array([mrg]*10), [10, 1])
-        ps = np.reshape(np.array([p]*10), [10,1])
+        # mrg = np.reshape(np.array([mrg] * 10), [10, 1])
+        mrg = np.reshape(mrg, [10, 1])
+        ps = np.reshape(np.array([p] * 10), [10, 1])
 
         acc_diff = acc_train - acc_test
 
         matrix = np.concatenate([acc_diff, mrg, ps], axis=1)
 
-        data = np.concatenate([data,matrix], axis=0)
-
-        print(data)
-
-
+        data = np.concatenate([data, matrix], axis=0)
     else:
         print("SKIP!")
     print("#")
 print("###")
 
-df = pd.DataFrame(data, columns=["Difference", "Margin", "p"])
+np.savetxt("data_wl_f.csv", data, delimiter=",")
 
-g = sns.lineplot(x="Margin", y="Difference", hue="p", data=df)
-g.set(xlabel="Margin $\lambda$", ylabel="Train - test accuracy", title="1-$\mathsf{WL}_\mathcal{F}$")
-
-sns.move_legend(g, "upper left", title='Prob.')
-
-plt.savefig('line_plot.pdf')
-
-#plt.show()
-
-
-
-exit()
+data = np.zeros([0, 3])
 
 # 1-WL.
 for (graph_db, classes, p, f) in datasets:
@@ -278,19 +252,23 @@ for (graph_db, classes, p, f) in datasets:
             gram_matrices.append(gram_matrix)
 
         acc_train, acc_test, mrg = linear_svm_evaluation(gram_matrices, classes, num_iter=1000, num_repetitions=10,
-                                                       C=[10 ** 10])
+                                                         C=[10 ** 10], all=False)
 
-        print(acc_train)
-        print(acc_test)
-        print(mrg)
-        results.append((p, acc_train, acc_test, mrg))
+        acc_train = np.reshape(acc_train, [10, 1])
+        acc_test = np.reshape(acc_test, [10, 1])
+        # mrg = np.reshape(np.array([mrg] * 10), [10, 1])
+        mrg = np.reshape(mrg, [10, 1])
+        ps = np.reshape(np.array([p] * 10), [10, 1])
+
+        acc_diff = acc_train - acc_test
+
+        matrix = np.concatenate([acc_diff, mrg, ps], axis=1)
+
+        data = np.concatenate([data, matrix], axis=0)
     else:
         print("SKIP!")
     print("#")
 
 print("###")
 
-for r in results:
-    print(r)
-
-
+np.savetxt("data_wl.csv", data, delimiter=",")
